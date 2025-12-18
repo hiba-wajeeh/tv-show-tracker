157983a5d814639ef0f78cc5350523eeb7637a46
Hiba Wajeeh <hibawajeeh1990@gmail.com>
2025-12-18 12:53:10 +0400

    Implemented Spring Boot backend running with test endpoint

---
+++ backend/src/main/java/com/hibawajeeh/tvtracker/backend/TvShowTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.yourname.tvtracker.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvShowTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TvShowTrackerApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/com/hibawajeeh/tvtracker/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibawajeeh.tvtracker.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
